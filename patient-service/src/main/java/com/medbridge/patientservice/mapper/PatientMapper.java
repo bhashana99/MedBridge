130bfd8f707f3e35742b20031beb9240ccdcc310
bhashana99 <bhashanachamodya99@gmail.com>
2025-05-23 15:45:42 +0530

    method for patientRequestDto convert to patient model

--- patient-service/src/main/java/com/medbridge/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/medbridge/patientservice/mapper/PatientMapper.java
@@ -1,7 +1,11 @@
 package com.medbridge.patientservice.mapper;
 
+import com.medbridge.patientservice.dto.PatientRequestDTO;
 import com.medbridge.patientservice.dto.PatientResponseDTO;
 import com.medbridge.patientservice.model.Patient;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toPatientResponseDTO(Patient patient){
@@ -13,7 +17,16 @@ public class PatientMapper {
         patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
 
         return patientResponseDTO;
+    }
 
+    public static Patient toPatientModel(PatientRequestDTO patientRequestDTO){
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
 
+        return patient;
     }
 }
