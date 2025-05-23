93d34ce52b81d5784057cca13d5891cca1664af4
bhashana99 <bhashanachamodya99@gmail.com>
2025-05-23 13:22:38 +0530

    create method for  getting patients

--- patient-service/src/main/java/com/medbridge/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/medbridge/patientservice/service/PatientService.java
@@ -1,13 +1,28 @@
 package com.medbridge.patientservice.service;
 
+import com.medbridge.patientservice.dto.PatientResponseDTO;
+import com.medbridge.patientservice.mapper.PatientMapper;
+import com.medbridge.patientservice.model.Patient;
 import com.medbridge.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PatientService {
     private PatientRepository patientRepository;
 
+
     public PatientService(PatientRepository patientRepository){
         this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients (){
+        List<Patient> patients = patientRepository.findAll();
+
+        List<PatientResponseDTO> patientResponseDTOS = patients.stream().map(
+                PatientMapper::toPatientResponseDTO).toList();
+
+        return  patientResponseDTOS;
     }
 }
