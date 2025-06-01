24448e2f02271d2ff8b90a119fadbe32469aa042
bhashana99 <bhashanachamodya99@gmail.com>
2025-06-01 16:32:00 +0530

    fix wrong method calling and end point

--- auth-service/src/main/java/com/medbridge/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/medbridge/authservice/controller/AuthController.java
@@ -6,10 +6,7 @@ import com.medbridge.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -37,12 +34,13 @@ public class AuthController {
         return ResponseEntity.ok((new LoginResponseDTO(token)));
     }
 
+    @GetMapping("/validate")
     public ResponseEntity<Void>  validateToken(@RequestHeader ("Authorization") String authHeader){
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
-        return authService.validToken(authHeader.substring(7))
+        return authService.validateToken(authHeader.substring(7))
                 ? ResponseEntity.ok().build()
                 : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
