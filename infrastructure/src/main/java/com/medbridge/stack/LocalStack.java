bbcec03cd61070f15421f0b102ff17fe9c25df91
bhashana99 <bhashanachamodya99@gmail.com>
2025-06-04 14:56:38 +0530

    create database

--- infrastructure/src/main/java/com/medbridge/stack/LocalStack.java
+++ infrastructure/src/main/java/com/medbridge/stack/LocalStack.java
@@ -2,7 +2,11 @@ package com.medbridge.stack;
 
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
 
 public class LocalStack extends Stack {
     private final Vpc vpc;
@@ -12,6 +16,12 @@ public class LocalStack extends Stack {
         super(scope,id,props);
 
         this.vpc = createVpc();
+
+        DatabaseInstance authServiceDb =
+                createDatabase("AuthServiceDB","auth-service-db");
+
+        DatabaseInstance patientServiceDb =
+                createDatabase("PatientServiceDB","patient-service-db");
     }
 
     private Vpc createVpc() {
@@ -20,6 +30,23 @@ public class LocalStack extends Stack {
                 .maxAzs(2)
                 .build();
     }
+
+    private DatabaseInstance createDatabase(String id,String dbName){
+        return DatabaseInstance.Builder
+                .create(this,id)
+                .engine(DatabaseInstanceEngine.postgres(
+                        PostgresInstanceEngineProps.builder()
+                                .version(PostgresEngineVersion.VER_17_2).build()
+                ))
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    }
+
 
     public static void main(final String[] args) {
         App app = new App(AppProps.builder().outdir("./cdk.out").build());
